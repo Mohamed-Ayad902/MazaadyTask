4eed86113ed2b0757d88bd6b3d75d92404d7b8c5
Mohamed-Ayad902 <mohamed.ayad7474@gmail.com>
2025-01-09 19:37:16 +0200

    tested the 3 main useCases (getCategories, getOptions, getProperties)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,13 @@ android {
         buildConfig = true
         viewBinding = true
     }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            merges += "META-INF/LICENSE.md"
+            merges += "META-INF/LICENSE-notice.md"
+        }
+    }
 }
 
 dependencies {
